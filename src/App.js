fae492332c94bb1fe5a3c093301a5535c51b049b
Elvis Diaz <elvisdiaz020@gmail.com>
2023-05-14 22:29:52 -0400

    adds image for tests and places top senetence in proper spot

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
+import pic from "./computer.jpeg";
+import style from "./style.css";
+const animation = () => {};
 function App() {
-  return <button>Hello</button>;
+  return (
+    <div>
+      <div id="top-sentence">
+        <p>Press This Button For a Suprise!!!</p>
+        <p id="top-arrow">^</p>
+      </div>
+
+      <button id="button">Press Here!!</button>
+      <img id="picture" src={pic} alt="Picture of a mother" />
+      <div id="bottom-left-sentence">
+        <p>^</p>
+        <p>Press This Button For a Suprise!!!</p>
+      </div>
+      <div>
+        <span id="bottom-right-sentence">
+          Press This Button For a Suprise!!!
+        </span>
+        <span>^</span>
+      </div>
+    </div>
+  );
 }
 
 export default App;
